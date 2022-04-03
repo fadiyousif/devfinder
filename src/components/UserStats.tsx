2366ccc6b2ebd1d282383d3aad376e868a0385bc
Fadi <fadi_bassam@hotmail.com>
2022-04-03 14:12:45 +0200

    create UserStats component

---
+++ src/components/UserStats.tsx
@@ -0,0 +1,31 @@
+import cx from "classnames"
+import { UserStatsItem } from "./UserStatsItem"
+
+export type UserStatsProps = {
+   publicRepos: number | null
+   followers: number | null
+   following: number | null
+   isLightTheme: boolean
+}
+
+export const UserStats = ({
+   publicRepos,
+   followers,
+   following,
+   isLightTheme,
+}: UserStatsProps) => {
+   const stats = [publicRepos, followers, following]
+
+   return (
+      <div className={cx("stats", { light: isLightTheme })}>
+         {stats.map((count, index) => (
+            <UserStatsItem
+               count={count}
+               index={index}
+               key={`stats-item-${index}`}
+               isLightTheme={isLightTheme}
+            />
+         ))}
+      </div>
+   )
+}
