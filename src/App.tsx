73fe29beabf4f026439476f1e348039a89326d81
Fadi <fadi_bassam@hotmail.com>
2022-04-03 14:23:14 +0200

    add hooks & logic in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,118 @@
-import React from 'react';
-import './App.css';
+import { useState, useEffect } from "react"
+import cx from "classnames"
+import { Header } from "./components/Header"
+import { Form } from "./components/Form"
+import { UserProfile } from "./components/UserProfile"
+import "./App.css"
 
-function App() {
+export const App = () => {
+   const [avatarUrl, setAvatarUrl] = useState("")
+   const [displayName, setDisplayName] = useState(null)
+   const [username, setUsername] = useState("")
+   const [joinedDate, setJoinedDate] = useState("")
+   const [bio, setBio] = useState(null)
+   const [publicRepos, setPublicRepos] = useState(null)
+   const [followers, setFollowers] = useState(null)
+   const [following, setFollowing] = useState(null)
+   const [location, setLocation] = useState(null)
+   const [twitterUsername, setTwitterUsername] = useState(null)
+   const [website, setWebsite] = useState("")
+   const [company, setCompany] = useState(null)
+
+   const [isLightTheme, setIsLightTheme] = useState(false)
+   const [isInvalidQuery, setIsInvalidQuery] = useState(false)
+
+   useEffect(() => {
+      fetchUser("octocat")
+   }, [])
+
+   const fetchUser = (user: string) => {
+      fetch(`https://api.github.com/users/${user}`)
+         .then((res) => {
+            if (res.ok) {
+               return res.json()
+            } else {
+               console.error(`${res.status} error`)
+               setIsInvalidQuery(true)
+               setTimeout(() => setIsInvalidQuery(false), 3000)
+            }
+         })
+         .then((data) => {
+            if (!data) {
+               return false
+            }
+            renderInfo(data)
+         })
+         .catch((err) => console.error(err))
+   }
+
+   const renderInfo = (data: any) => {
+      const {
+         avatar_url,
+         login: username,
+         name,
+         created_at,
+         bio,
+         public_repos,
+         followers,
+         following,
+         location,
+         twitter_username,
+         blog: website,
+         company,
+      } = data
+
+      setAvatarUrl(avatar_url)
+      setUsername(username)
+      setDisplayName(name)
+      setJoinedDate(created_at)
+      setBio(bio)
+      setPublicRepos(public_repos)
+      setFollowers(followers)
+      setFollowing(following)
+      setLocation(location)
+      setTwitterUsername(twitter_username)
+      setWebsite(website)
+      setCompany(company)
+   }
+
+   const renderDate = () => {
+      const [month, day, year] = new Date(joinedDate)
+         .toDateString()
+         .split(" ")
+         .slice(1)
+      return `Joined ${day} ${month} ${year}`
+   }
+
    return (
-      <div className="App">
-         <header className="App-header">
-            <p>
-               Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-               className="App-link"
-               href="https://reactjs.org"
-               target="_blank"
-               rel="noopener noreferrer"
-            >
-               Learn React
-            </a>
-         </header>
+      <div className={cx("app", { light: isLightTheme })}>
+         <div className="container">
+            <Header
+               isLightTheme={isLightTheme}
+               setIsLightTheme={setIsLightTheme}
+            />
+            <Form
+               fetchUser={fetchUser}
+               isInvalidQuery={isInvalidQuery}
+               isLightTheme={isLightTheme}
+            />
+            <UserProfile
+               username={username}
+               displayName={displayName}
+               joinedDate={joinedDate}
+               bio={bio}
+               publicRepos={publicRepos}
+               followers={followers}
+               following={following}
+               location={location}
+               twitterUsername={twitterUsername}
+               website={website}
+               company={company}
+               avatarUrl={avatarUrl}
+               renderDate={renderDate}
+               isLightTheme={isLightTheme}
+            />
+         </div>
       </div>
-   );
+   )
 }
-
-export default App;
