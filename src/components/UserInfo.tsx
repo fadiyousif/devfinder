c788e86ae79d392f55e3657c9e44fc2487055e1b
Fadi <fadi_bassam@hotmail.com>
2022-04-03 14:15:25 +0200

    create UserInfo component

---
+++ src/components/UserInfo.tsx
@@ -0,0 +1,32 @@
+import { UserInfoItem } from "./UserInfoItem"
+
+export type UserInfoProps = {
+   location: string | null
+   twitterUsername: string | null
+   website: string
+   company: string | null
+   isLightTheme: boolean
+}
+
+export const UserInfo = ({
+   location,
+   twitterUsername,
+   website,
+   company,
+   isLightTheme,
+}: UserInfoProps) => {
+   const info = [location, twitterUsername, website, company]
+
+   return (
+      <div className="user-info">
+         {info.map((item, index) => (
+            <UserInfoItem
+               item={item}
+               index={index}
+               key={`info-item-${index}`}
+               isLightTheme={isLightTheme}
+            />
+         ))}
+      </div>
+   )
+}
